1a5d07e8bdfdf0b9b003497575b436911ab515e5
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-04-09 07:34:07 +0700

    Update and rename plugins/viadana.js to plugin/viadana.js

--- plugin/viadana.js
+++ plugin/viadana.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Digi
-╠➥ *60149431385*
+╠➥ *6285759662798*
 ║- Owner Bot:
-║- wa.me/60189830350
-╠═〘 LYNXZY 〙 ═
+║- wa.me/6285759662798
+╠═〘 LαrαsChαnn 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
